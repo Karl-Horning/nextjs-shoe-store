675558958edd2e77bd3306f41e9991ba14a7181b
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-06-01 19:54:36 +0100

    Change filters, remove ShoeCardList props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Header from "@/components/Header/Header";
 import ShoeCardList from "@/components/ShoeCard/ShoeCardList";
-import { getAllBrands, getAllShoes } from "@/data/data";
 
 export default async function Home() {
-    const allShoes = await getAllShoes();
-    const allBrands = await getAllBrands();
-
     return (
         <main>
             <Header />
-            <ShoeCardList shoeData={allShoes} shoeBrands={allBrands} />
+            <ShoeCardList />
         </main>
     );
 }
--- src/components/ShoeCard/ShoeCardList.tsx
+++ src/components/ShoeCard/ShoeCardList.tsx
@@ -1,42 +1,87 @@
 "use client";
 
-import { Select, SelectItem } from "@nextui-org/select";
+import React, { useState, useEffect } from "react";
+import {
+    getAllBrands,
+    getAllShoes,
+    getShoesByBrand,
+    getLeastExpensiveShoes,
+    getMostExpensiveShoes,
+    Shoe,
+} from "@/data/data";
 import ShoeCard from "@/components/ShoeCard/ShoeCard";
 import { Button } from "@nextui-org/react";
-import { Shoe } from "@/data/data";
-
-/**
- * Represents the shoe card list properties.
- *
- * @typedef {Object} ShoeCardListProps
- * @property {Object} params - The parameters of the shoe card.
- * @property {string} params.shoeData - The data for the shoes to be displayed.
- * @property {string} params.shoeBrands - The list of shoe brands for filtering.
- */
-interface ShoeCardListProps {
-    shoeData: Shoe[];
-    shoeBrands: string[];
-}
 
 /**
  * A component that displays a list of shoe cards and filtering options.
  *
- * @param {ShoeCardListProps} shoeCardListProps - The shoe card list properties containing the params.
  * @returns {JSX.Element} - The ShoeCardList component.
  */
-export default function ShoeCardList({
-    shoeData,
-    shoeBrands,
-}: ShoeCardListProps) {
+export default function ShoeCardList() {
+    const [shoes, setShoes] = useState<Shoe[]>([]);
+    const [brands, setBrands] = useState<string[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const allShoes = await getAllShoes();
+            const allBrands = await getAllBrands();
+            setShoes(allShoes);
+            setBrands(allBrands);
+        };
+
+        fetchData();
+    }, []);
+
+    const handleButtonClick = async (
+        e: React.MouseEvent<HTMLButtonElement>
+    ) => {
+        const buttonId = e.currentTarget.id;
+
+        let shoesFilter: Shoe[] = [];
+        switch (buttonId) {
+            case "all-shoes-btn":
+                shoesFilter = await getAllShoes();
+                break;
+            case "popular-btn":
+                shoesFilter = await getLeastExpensiveShoes();
+                break;
+            case "featured-btn":
+                shoesFilter = await getMostExpensiveShoes();
+                break;
+            case "brand-btn":
+                shoesFilter = await getShoesByBrand('Nike');
+                break;
+            default:
+                break;
+        }
+
+        setShoes(shoesFilter);
+    };
+
     return (
         <section id="shoe-list" className="container mx-auto min-w-[375px]">
-            <div className="mb-5 grid-cols-3 gap-4 md:grid">
+            <div className="mb-5 grid-cols-4 gap-4 md:grid">
                 <div className="flex items-center justify-center">
                     <Button
-                        variant="light"
-                        size="lg"
-                        color="secondary"
                         className="mb-5 w-full"
+                        color="secondary"
+                        id="all-shoes-btn"
+                        onClick={handleButtonClick}
+                        size="lg"
+                        variant="ghost"
+                    >
+                        All Shoes
+                    </Button>
+                </div>
+
+                <div className="flex items-center justify-center">
+                    <Button
+                        className="mb-5 w-full"
+                        color="secondary"
+                        id="popular-btn"
+                        onClick={handleButtonClick}
+                        size="lg"
+                        variant="light"
                     >
                         Popular
                     </Button>
@@ -44,28 +89,33 @@ export default function ShoeCardList({
 
                 <div className="flex items-center justify-center">
                     <Button
-                        variant="light"
-                        size="lg"
-                        color="secondary"
                         className="mb-5 w-full"
+                        color="secondary"
+                        id="featured-btn"
+                        onClick={handleButtonClick}
+                        size="lg"
+                        variant="light"
                     >
                         Featured
                     </Button>
                 </div>
 
-                <Select
-                    className="mb-5"
-                    label="Filter by brand"
-                    placeholder="All"
-                >
-                    {shoeBrands?.map((brand) => (
-                        <SelectItem key={brand}>{brand}</SelectItem>
-                    ))}
-                </Select>
+                <div className="flex items-center justify-center">
+                    <Button
+                        className="mb-5 w-full"
+                        color="secondary"
+                        id="brand-btn"
+                        onClick={handleButtonClick}
+                        size="lg"
+                        variant="light"
+                    >
+                        Nike
+                    </Button>
+                </div>
             </div>
 
             <div className="grid justify-items-center gap-4 sm:grid-cols-2 md:grid-cols-3">
-                {shoeData.map((shoe) => (
+                {shoes.map((shoe) => (
                     <ShoeCard
                         key={shoe.ShoeId}
                         id={shoe.ShoeId}
